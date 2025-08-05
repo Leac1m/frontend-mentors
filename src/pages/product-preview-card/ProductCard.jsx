61afe3b2dcc675f1db582cb0cf2eca010fda9dbd
leac1m <obemichael10@gmail.com>
2025-08-05 01:04:47 +0100

    worked on product card

--- src/pages/product-preview-card/ProductCard.jsx
+++ src/pages/product-preview-card/ProductCard.jsx
@@ -9,7 +9,7 @@ const ProductCard = () => {
       <div className="product-card-main">
       <title>Frontend Mentor | Product preview card component</title>
       <picture >
-        <source className="main-img" media="(min-width: 500px)" srcset={DesktopProductImage}  />
+        <source className="main-img" media="(min-width: 600px)" srcSet={DesktopProductImage}  />
         <img className="main-img" src={MobileProductImage} alt="Product preview image" />
       </picture>
       <div className="text-area">
@@ -26,7 +26,8 @@ const ProductCard = () => {
 
         <button><img src={Cart} alt="cart image"></img>Add to Cart</button>
       </div>
-  </div>
+
+    </div>
       <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
         Coded by <a href="#">Michael Obe</a>.
